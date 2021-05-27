815dcf6d2d74508ec3dc3b021ed2c94378f94447
simonsymhoven <post@simon-symhoven.de>
2021-05-27 09:49:19 +0200

    wrap interfaces in abstract classes

--- src/main/java/io/jenkins/plugins/monitoring/MonitorPortlet.java
+++ src/main/java/io/jenkins/plugins/monitoring/MonitorPortlet.java
@@ -3,58 +3,48 @@ package io.jenkins.plugins.monitoring;
 import java.util.Optional;
 
 /**
- * This interface defines the portlet to be shown in the monitoring dashboard.
+ * <p>Implements the {@link IMonitorPortlet} interface and defines a single portlet for the pull request monitoring
+ * dashboard.</p>
+ *
+ * <p>The id must be unique. Please make sure that the id is related to the plugin so that
+ * there are no conflicts with other plugins. It is recommended to use the artifact id
+ * of the plugin or parts of it as prefix. If several portlets of the same class are created in the
+ * {@link MonitorPortletFactory}, it must be ensured that {@link #getId()} is always unique for each portlet!</p>
+ *
+ * @see io.jenkins.plugins.monitoring.IMonitorPortlet
+ * @since 1.6.0
+ * @author Simon Symhoven
  */
-public interface MonitorPortlet {
+public abstract class MonitorPortlet implements IMonitorPortlet {
 
-    /**
-     * Defines the the to be shown.
-     *
-     * @return
-     *          the title.
-     */
-    String getTitle();
+    @Override
+    public String getTitle() {
+        return "Monitor Portlet";
+    }
 
-    /**
-     * Defines the id for the portlet.
-     *
-     * @return
-     *          the id.
-     */
-    String getId();
+    @Override
+    public String getId() {
+        return "monitor-portlet";
+    }
 
-    /**
-     * Defines the preferred width of the portlet.
-     *
-     * @return
-     *          the width in pixels.
-     */
-    int getPreferredWidth();
+    @Override
+    public int getPreferredWidth() {
+        return 100;
+    }
 
-    /**
-     * Defines the preferred height of the portlet.
-     *
-     * @return
-     *          the height in pixels.
-     */
-    int getPreferredHeight();
+    @Override
+    public int getPreferredHeight() {
+        return 100;
+    }
 
-    /**
-     * Defines the icon to show in the dropdown list of available portlets.
-     *
-     * @return
-     *          the app relative icon url depending on ${resURL} (/static/cache-id),
-     *          or {@code Optional.empty()}, if a default icon should be added.
-     */
-    Optional<String> getIconUrl();
+    @Override
+    public Optional<String> getIconUrl() {
+        return Optional.empty();
+    }
 
-    /**
-     * Defines the relative url to a detail view of the portlet.
-     *
-     * @return
-     *          the relative link to the detail view depending on the current build url
-     *          (e.g. http://localhost:8080/jenkins/job/job-name/view/change-requests/job/PR-1/1/),
-     *          or {@code Optional.empty()}, if no link should be added to portlet.
-     */
-    Optional<String> getDetailViewUrl();
+    @Override
+    public Optional<String> getDetailViewUrl() {
+        return Optional.empty();
+    }
+
 }
--- src/main/java/io/jenkins/plugins/monitoring/MonitorPortletFactory.java
+++ src/main/java/io/jenkins/plugins/monitoring/MonitorPortletFactory.java
@@ -4,28 +4,36 @@ import hudson.ExtensionPoint;
 import hudson.model.Run;
 
 import java.util.Collection;
+import java.util.Collections;
 
 /**
- * Factory for {@link MonitorPortlet}.
+ * <p>Implements the {@link IMonitorPortletFactory} interface and defines the {@link ExtensionPoint} to register a
+ * new pull request monitoring portlet.</p>
+ *
+ * <p>The {@link #getDisplayName()} is shown in a dropdown list as optgroup. The children of the optgroup
+ * are the registered portlets of {@link #getPortlets(Run)}. </p>
+ *
+ * <p>Since an empty dashboard is always added by default, it is possible that the method {@link #getPortlets(Run)} will
+ * be called even though the current run may not be finished. It is therefore advisable to perform a null check on
+ * the actions of the run required by your portlet and return an empty list if necessary.
+ * (Example: <a href="https://github.com/simonsymhoven/code-coverage-api-plugin/blob/pull-request-monitoring-portlet/
+ * src/main/java/io/jenkins/plugins/coverage/CoveragePullRequestMonitoringPortlet.java#L142">code-coverage-api</a>)</p>
+ *
+ * @see io.jenkins.plugins.monitoring.IMonitorPortletFactory
+ * @since 1.6.0
+ * @author Simon Symhoven
  */
-public interface MonitorPortletFactory extends ExtensionPoint {
+public abstract class MonitorPortletFactory implements ExtensionPoint, IMonitorPortletFactory {
 
-    /**
-     * Get a collection of {@link MonitorPortlet} to display.
-     *
-     * @param build
-     *              the reference {@link Run}.
-     *
-     * @return
-     *              a collection of {@link MonitorPortlet}.
-     */
-    Collection<MonitorPortlet> getPortlets(Run<?, ?> build);
+    @Override
+    public Collection<MonitorPortlet> getPortlets(Run<?, ?> build) {
+        return Collections.emptyList();
+    }
 
-    /**
-     * Defines the name of the factory.
-     *
-     * @return
-     *              the name to display for the factory.
-     */
-    String getDisplayName();
+
+    @Override
+    public String getDisplayName() {
+        return "Monitor Portlet Factory";
+    }
+
 }
